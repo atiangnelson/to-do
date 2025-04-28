e9ee838c16202af48a740ddf91b51d94270076ce
Nelson <nelson.atiang@student.moringaschool.com>
2025-04-28 21:09:47 +0300

    added he tasklist component

--- src/Tasklist.jsx
+++ src/Tasklist.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Tasklist = () => {
+const Tasklist = ({tasks,onToogle,onDelete,onEdit}) => {
   return (
-    <div>Tasklist</div>
+    <div>
+        <ul>
+            {tasks.map((task)=>(
+                <li key={task.id} style={{opacity : task.completed? 0.5:1}}/>
+            ))}
+        </ul>
+
+    </div>
   )
 }
 
