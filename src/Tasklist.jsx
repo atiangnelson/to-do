2a3f686fc73ad1dbc476f8f52f8cb95c61c0c02a
Nelson <nelson.atiang@student.moringaschool.com>
2025-04-28 21:42:24 +0300

    added usestate for edditing thre id of a task

--- src/Tasklist.jsx
+++ src/Tasklist.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const Tasklist = ({tasks,onToogle,onDelete,onEdit}) => {
+const Tasklist = ({tasks,onToggle,onDelete,onEdit}) => {
+    const [editingId,setEditingId]=useState(null)
   return (
     <div>
         <ul>
             {tasks.map((task)=>(
-                <li key={task.id} style={{opacity : task.completed? 0.5:1}}/>
+                <li key={task.id} style={{opacity : task.completed? 0.5:1}}>
+                    <input type='checkbox' checked={task.completed} onChange={()=>onToggle(task.id,task.completed)} />
+                    <span
+                    style={{textDecoration:task.completed? "line through":"none"}}
+                    >
+                        
+                    </span>
+                </li>
             ))}
         </ul>
 
